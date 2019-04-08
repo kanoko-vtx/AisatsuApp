37a0ed0ae545a2a21b3b9d0f46a0534b5a428d55
kanoko-vtx <a,kanoko@vortex-gi.com>
2019-04-08 16:35:28 +0900

    最初のコミット レイアウトまで

---
+++ app/src/main/java/kanoko/akira/techacademy/aisatsuapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package kanoko.akira.techacademy.aisatsuapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.app.TimePickerDialog
+import android.support.v7.app.AlertDialog
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
