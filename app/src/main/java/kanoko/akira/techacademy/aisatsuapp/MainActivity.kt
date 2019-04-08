0559f4e77caefa02f8b23f20ff1a75db25b34814
kanoko-vtx <a,kanoko@vortex-gi.com>
2019-04-08 22:21:05 +0900

    時間によってメッセージをTextViewに表示できるところまで

--- app/src/main/java/kanoko/akira/techacademy/aisatsuapp/MainActivity.kt
+++ app/src/main/java/kanoko/akira/techacademy/aisatsuapp/MainActivity.kt
@@ -8,6 +8,8 @@ import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.app.DatePickerDialog
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
 
         Button.setOnClickListener(this)
+
     }
 
     override fun onClick(v: View) {
@@ -30,17 +33,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             TimePickerDialog.OnTimeSetListener { _, hour, minute ->
                 Log.d("Aisatsu", "$hour:$minute")
 
-                if (hour in 0..1)
+                if (hour in 0..1) {
                     Log.d("Aisatsu", "こんばんは")
+                    Aisatsu.text = "こんばんは" }
 
-                if (hour in 2..9)
+                if (hour in 2..9) {
                     Log.d("Aisatsu", "おはよう")
+                    Aisatsu.text = "おはよう" }
 
-                if (hour in 10..17)
+                if (hour in 10..17) {
                     Log.d("Aisatsu", "こんにちは")
+                    Aisatsu.text = "こんにちは" }
 
-                if (hour in 18..24)
+                if (hour in 18..24) {
                     Log.d("Aisatsu", "こんばんは")
+                    Aisatsu.text = "こんばんは" }
 
             },
             13, 0, true)
