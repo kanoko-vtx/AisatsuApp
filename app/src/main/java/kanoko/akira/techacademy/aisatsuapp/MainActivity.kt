4f3bf7a2f8352a359544e9697298d8aa7c462461
kanoko-vtx <a,kanoko@vortex-gi.com>
2019-04-08 18:19:47 +0900

    TimePickerDialogが表示されて 設定した値がログに出力されるまで

--- app/src/main/java/kanoko/akira/techacademy/aisatsuapp/MainActivity.kt
+++ app/src/main/java/kanoko/akira/techacademy/aisatsuapp/MainActivity.kt
@@ -1,18 +1,36 @@
 package kanoko.akira.techacademy.aisatsuapp
 
+import android.app.TimePickerDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.app.TimePickerDialog
 import android.support.v7.app.AlertDialog
 import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import android.app.DatePickerDialog
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Button.setOnClickListener(this)
     }
 
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.Button -> showTimePickerDialog()
+        }
+    }
+
+    private fun showTimePickerDialog() {
+        val timePickerDialog = TimePickerDialog(
+            this,
+            TimePickerDialog.OnTimeSetListener { _, hour, minute ->
+                Log.d("Aisatsu", "$hour:$minute")
+            },
+            13, 0, true)
+        timePickerDialog.show()
+    }
 }
